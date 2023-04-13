29f99c94f4018be573efa2b5a8385b0ea0d6ae43
DogsareVeryVeryVeryVerycool <111755061+DogsareVeryVeryVeryVerycool@users.noreply.github.com>
2023-04-13 07:59:57 -0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,22 @@
+function addUser()
+{
+  user_name = document.getElementById("user_name").value
+
+  localStorage.setItem("user_name",);
+
+  window.location = "kwitter_room.html";
+}
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDrye6HjOt56PqZGbPZsBzSWEDO01S4VY",
+  authDomain: "projecat-93.firebaseapp.com",
+  databaseURL: "https://projecat-93-default-rtdb.firebaseio.com",
+  projectId: "projecat-93",
+  storageBucket: "projecat-93.appspot.com",
+  messagingSenderId: "838754316246",
+  appId: "1:838754316246:web:741d06d9bfa2a5a9e1fe77",
+  measurementId: "G-1Y5RMTEJM1"
+};
+
+
